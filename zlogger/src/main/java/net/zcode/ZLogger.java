ca866c53820ef361e7ebeb99cab4252fdbbe9e6d
Bartosz Hamera <cookiedesign.fx@gmail.com>
2024-02-19 12:41:53 +0000

    Color background:>

--- zlogger/src/main/java/net/zcode/ZLogger.java
+++ zlogger/src/main/java/net/zcode/ZLogger.java
@@ -2,6 +2,12 @@ package net.zcode;
 import net.zcode.enums.Types.Code;
 
 public class ZLogger {
+
+    public static final String ANSI_RESET = "\u001B[0m"; 
+    public static final String ANSI_GREEN_BG = "\u001B[42m";
+    public static final String ANSI_RED_BG = "\u001B[41m";
+    public static final String ANSI_BLUE_BG = "\u001B[44m";
+
     public static void main(String[] args) {
         System.out.println("[ ZLogger | Build: 1.0.1 | Cosmic ]");
 
@@ -15,13 +21,13 @@ public class ZLogger {
     public static void LOG(Code type, String text){
         switch (type){
             case INFO:
-                System.out.println("~ ZLogger | INFO: " + text);
+                System.out.println(ANSI_BLUE_BG + "~ ZLogger | INFO: " + text + ANSI_RESET);
                 break;
             case ERROR:
-                System.out.println("~ ZLogger | ERROR: " + text);
+                System.out.println(ANSI_RED_BG + "~ ZLogger | ERROR: " + text + ANSI_RESET);
                 break;
             case SUCCESS:
-                System.out.println("~ ZLogger | SUCCESS: " + text);
+                System.out.println(ANSI_GREEN_BG + "~ ZLogger | SUCCESS: " + text + ANSI_RESET);
                 break;
         }
     }
